9e7c2e19523c087d589296047f4db9cf4bd5d1d3
milana-a <82338671+milana-a@users.noreply.github.com>
2022-12-09 19:05:49 +0100

    Add files via upload

---
+++ README My Project Webpage Fotoausstellung.md
@@ -0,0 +1,8 @@
+1. This is a link to figma file - my idea of a page 
+https://www.figma.com/proto/wVcn7TiaHD3MfwyjYdepQf/Figma-%C3%9Cbung?node-id=107%3A117&scaling=scale-down&page-id=107%3A115&starting-point-node-id=107%3A117
+
+2. You can run the html file "index" and than Button "Fotoausstellung"
+
+3. Files that I created for this project are fotoausstellung.html  fotoausstellung.js and mystylesFotoausstellung.css
+
+4. Thank you  )))
---
+++ fotoausstellung.js
@@ -0,0 +1,95 @@
+// I use Glider library to show my fotos
+//
+//
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
+
+  // I show an overlay with a big version of foto on click
+  //
+  //
+  const initClickHandler = () => {
+    const imageOverview = document.querySelectorAll('.zoomable');
+    imageOverview.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      const image = event.target.querySelector('img');
+      const src = image.getAttribute('src'); 
+      createOverlay(src);
+      });
+  })}; 
+   initClickHandler();
+  const createOverlay = (src) => {
+    const fullPage = document.querySelector('#fullpage');
+    fullPage.style.backgroundImage = "url('" + src + "')";
+    fullPage.style.display = 'block';
+  };
+
+// I found an API with random Cats fotos and use a random foto for a comment. 
+// By a click on the button "Bild ändern" you can get another random foto of a cat
+
+const changeFoto = document.querySelector('#bildNew');
+changeFoto.addEventListener('click', (event) => {
+const url = `https://api.thecatapi.com/v1/images/search`;  
+fetch(url)
+.then((response) => {
+return response.json();
+})
+.then((data) => {
+  let imagesData = data;
+  imagesData.map(function(imageData) {
+    
+    let image = document.getElementById('fotoKomment');
+    //use the url from the image object
+    image.src = `${imageData.url}`;
+    });
+})
+.catch(function(error) {
+   console.log(error);
+});
+  
+});
+});
+
+// I take data from the form and create a comment in DOM
+
+function submitKomment(theForm) {
+  console.log(theForm);
+  const Vorname = theForm.vorname.value;
+  const Nachname = theForm.nachname.value;
+  const Kommentar = theForm.frage2.value;
+  const rad = theForm.country.value;
+  const email = theForm.email.value;
+
+  // I ask user to tell about the contry they are from
+  //
+  //
+  if (!rad) { 
+    alert('Enter Location');
+   }
+
+   // I take the Template, copy it and put data from the form
+   //
+   // 
+  else {
+  const MyDivKomment = document.querySelector('#kommentFormTemplate');
+  var clone = MyDivKomment.cloneNode(true);
+  clone.id='kommentForm';
+
+  clone.querySelector('#KommentorName').innerHTML = Vorname;
+  clone.querySelector('#KommentarText').innerHTML = rad;
+  clone.querySelector('#KommentorSecondName').innerHTML = Nachname;
+  clone.querySelector('#KommentarMy').innerHTML = Kommentar;
+  clone.querySelector('#thisFoto').src = theForm.querySelector('#fotoKomment').src;
+  document.querySelector('#CommentList').appendChild(clone);
+  return false;
+  }
+  } 
+
